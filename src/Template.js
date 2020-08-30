cd80e234faa5d5b7b333bfc249d6d0ec809d5978
Mary Vu <vuumary@gmail.com>
2020-08-30 12:22:30 -0700

    commented template :)

---
+++ src/Template.js
@@ -0,0 +1,69 @@
+// this is a single line comment! try writing one of your own below:
+// if you are ever waiting for the class to catch up,
+// i want you to be commenting your code :)
+
+// write 2-3 print statements below:
+
+
+// try using a method below:
+                
+
+// uh oh, we can't really see that value... so we have to print it!
+// do you remember how to print? try printing it yourself below these comments
+// hint: put the Math.random() into the parenthesis of the print statement
+/* i space one the parenthesis for the kids to see and not get a syntax error, but this
+isnt something they have to do */
+
+
+
+// adding strings together is called "concatenation"
+// you can add strings together like this:
+
+
+// variables types: String, Boolean, Integer, Double
+// create a variable for each type below and have at least one "let" & "const"
+
+
+
+
+
+
+// print 1 or more of your variables below:
+
+
+
+// arithmetic operations: addition (+), subtraction (-), division (/) 
+// use each arithemetic operator by printings or setting it to a variable below:
+
+
+
+
+
+
+// create an if-else statements below:
+
+
+
+
+
+
+
+
+
+// create at least one function below:
+
+
+
+
+
+
+
+
+
+
+// call your function(s) and print to prove they work below:
+
+
+
+
+
